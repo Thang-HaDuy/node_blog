538e486ce75411505aedbfc18972c963692fcf49
Thang-HaDuy <haduythang871@gmail.com>
2022-11-09 14:31:44 +0700

    doan cuoi

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -21,7 +21,6 @@ class CourseController {
     // [POST] /courses/store
     store(req, res, next) {
         req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
-        //req.body._id = 1;
         const course = new Course(req.body);
         course
             .save()
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
 const mongooseDelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 
 const Course = new Schema(
     {
-        //_id: {type:Number, },
+        _id: {type:Number, },
         name: { type: String, required: true },
         description: { type: String },
         image: { type: String },
@@ -15,13 +16,15 @@ const Course = new Schema(
         slug: { type: String, slug: 'name', unique: true },
     },
     {
-       // _id: false,
+        _id: false,
         timestamps: true,
     },
 );
 
 // Add plugins
 mongoose.plugin(slug);
+
+Course.plugin(AutoIncrement);
 Course.plugin(mongooseDelete, {
     deletedAt: true,
     overrideMethods: 'all',
